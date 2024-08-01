6cfabd8f8ca292a23c9c6f1ca7cbce5238e1add0
at0x123 <vp.ca94@gmail.com>
2024-08-01 16:54:00 -0500

    changes

---
+++ nerd_base/src/app.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import { createBot, MemoryDB, createProvider, createFlow } from '@builderbot/bot'
+import { TelegramProvider } from '@builderbot-plugins/telegram'
+
+const main = async () => {
+ //   const { flow } = await createShopifyFlow()
+
+    await createBot({
+        database: new MemoryDB(),
+        provider: createProvider(TelegramProvider),
+        flow: createFlow(flow)
+    })
+
+}
+
+main()
